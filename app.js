e04dc5ae44ffc11434a5f38e74c0d91b63b5cd47
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-02-10 20:29:09 -0300

    feat: repositorio base

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
+const {title} = require('process');
 const app = express();
 const port = 3000;
 
 // Middleware para parsear o corpo das requisições
 app.use(express.json());
 
-// Rota de teste
+// Configurar o Express para usar EJS como template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Middleware para arquivos estáticos (CSS, JS, imagens)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota main
 app.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, 'src/views', 'index.html'))
+    res.render('index', {title: "home", page: "pages/home" })
+});
+
+// Rota sobre
+app.get("/sobre", (_, res) => {
+    res.render('index', { title: "sobre", page: "pages/sobre" })
 });
 
 // Iniciar o servidor
